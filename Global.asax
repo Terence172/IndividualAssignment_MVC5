89aca2c2ddf504f2408354ebcb51302aa13727d7
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-07-03 13:37:16 +0800

    Add project files.

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IndividualAssignment_MVC5.MvcApplication" Language="C#" %>
---
+++ Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(IndividualAssignment_MVC5.Startup))]
+namespace IndividualAssignment_MVC5
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
