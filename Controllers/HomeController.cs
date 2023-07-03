cf57ffcc518e353288a63c294ff5167a4d7df623
Terence Nathan <terenceloorthanathan43@gmail.com>
2023-07-03 15:12:06 +0800

    Updated  a lot of stuff

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using IndividualAssignment_MVC5.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,23 +9,87 @@ namespace IndividualAssignment_MVC5.Controllers
 {
     public class HomeController : Controller
     {
+        private db_individualTestEntities db = new db_individualTestEntities();
+        
         public ActionResult Index()
         {
             return View();
         }
 
-        public ActionResult About()
+        //Dashboard blah blah
+        public ActionResult DashboardAdmin()
         {
-            ViewBag.Message = "Your application description page.";
+            if (Session["UserID"] == null)
+            {
+                ViewBag.user = db.users.Count();
+                return RedirectToAction("Index", "Login");
+            }
+
+            if (Session["UserType"].ToString() == "Lecturer")
+            {
+                return RedirectToAction("DashboardLecturer", "Home");
+            }
+
+            if (Session["UserType"].ToString() == "Admin")
+            {
+                return RedirectToAction("DashboardAdmin", "Home");
+            }
+
+
+            //ViewBag.tenantcount = db.tenants.Count();
+            //ViewBag.landlords = db.landlords.Count();
+            //ViewBag.room = db.rooms.Count();
+            //ViewBag.availableRoom = db.rooms.Count(r => r.room_status == "Available");
+            //ViewBag.bookedRoom = db.rooms.Count(r => r.room_status == "Booked");
+
+
+            //var availableRooms = db.rooms.Where(r => r.room_status == "Available").ToList();
+
+            // Pass the available rooms to the view
+            return View();
+        }
+
+        public ActionResult DashboardLecturer()
+        {
+            if (Session["UserID"] == null)
+            {
+                ViewBag.user = db.users.Count();
+                return RedirectToAction("Index", "Login");
+            }
+
+            if (Session["UserType"].ToString() == "Student")
+            {
+                return RedirectToAction("DashboardStudent", "Home");
+            }
+
+            if (Session["UserType"].ToString() == "Admin")
+            {
+                return RedirectToAction("DashboardAdmin", "Home");
+            }
 
             return View();
         }
 
-        public ActionResult Contact()
+        public ActionResult DashboardStudent()
         {
-            ViewBag.Message = "Your contact page.";
+            if (Session["UserID"] == null)
+            {
+                ViewBag.user = db.users.Count();
+                return RedirectToAction("Index", "Login");
+            }
+
+            if (Session["UserType"].ToString() == "Lecturer")
+            {
+                return RedirectToAction("DashboardLecturer", "Home");
+            }
+
+            if (Session["UserType"].ToString() == "Admin")
+            {
+                return RedirectToAction("DashboardAdmin", "Home");
+            }
 
             return View();
         }
+
     }
 }
